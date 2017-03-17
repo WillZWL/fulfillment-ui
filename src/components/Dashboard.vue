e973510787332ab62acb4beb733ade7eb0a2e148
Will <will.zhang@eservicesgroup.com>
2017-03-17 13:49:38 +0800

    fulfillment dashboard page

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -19,47 +19,144 @@
     <div class="dashboard">
       <a class="btn btn-primary pull-right" href="http://admincentre.eservicesgroup.com/order/integrated_order_fulfillment/iwms_allocation_plan">Allocation Plan</a>
       <h1>Dashboard</h1>
-      <div class="panel panel-info">
-        <div class="panel-heading">Orders</div>
-        <div class="panel-body">
-          <p>This part just a demo now, it will comming soon, please wait</p>
-          <div class="alert alert-danger" role="alert">
-            <strong>Pending Orders  1000 orders</strong> Those orders need to allocate.
+      <div>
+        <div class="col-md-8 col-sm-12">
+          <div class="panel panel-info">
+            <div class="panel-heading">Orders Count</div>
+            <div class="panel-body">
+              <div class="alert alert-danger" role="alert">
+                <strong>Pending Paid Orders {{ pending_paid_orders_count }} </strong> Those orders need to do allocation
+              </div>
+              <div class="alert alert-success" role="alert">
+                <strong>All Paid Orders {{ all_paid_orders_count }} </strong> Those orders has been paid
+              </div>
+              <div class="alert alert-info" role="alert">
+                <strong>Allocated Orders {{ allocated_orders_count }} </strong> Those orders has been allocated.
+              </div>
+            </div>
           </div>
-          <div class="alert alert-success" role="alert">
-            <strong>Allocated Orders 800 orders</strong> Those orders has been allocated.
+          <div class="panel panel-primary">
+            <div class="panel-heading">Merchant Orders Count</div>
+            <div class="panel-body">
+              <div class="col-md-4 col-sm-12">
+                <div class="panel panel-danger">
+                  <div class="panel-heading">Pending Paid Orders</div>
+                  <div class="panel-body">
+                     <p>{{ loading }}</p>
+                    <ul class="list-group">
+                      <li class="list-group-item" v-for="merchant_pending in merchant_pending_orders_count">
+                         <span class="badge">{{ merchant_pending.count }}</span>
+                         {{ merchant_pending.merchant_id }}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-12">
+                <div class="panel panel-success">
+                  <div class="panel-heading">All Paid Orders</div>
+                  <div class="panel-body">
+                    <p>{{ loading }}</p>
+                    <ul class="list-group">
+                      <li class="list-group-item" v-for="merchant_all_paid in merchant_all_paid_orders_count">
+                         <span class="badge">{{ merchant_all_paid.count }}</span>
+                         {{ merchant_all_paid.merchant_id }}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-12">
+                <div class="panel panel-info">
+                  <div class="panel-heading">Allocated Orders</div>
+                  <div class="panel-body">
+                    <p>{{ loading }}</p>
+                    <ul class="list-group">
+                      <li class="list-group-item" v-for="merchant_allocated in merchant_allocated_orders_count">
+                         <span class="badge">{{ merchant_allocated.count }}</span>
+                         {{ merchant_allocated.merchant_id }}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="panel panel-danger">
-        <div class="panel-heading">Pending Orders</div>
-        <div class="panel-body">
-          <p>This part just a demo now, it will comming soon, please wait</p>
-          <div class="progress">
-            <span>3 Doodler &nbsp;&nbsp;&nbsp; 150 orders</span>
-            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"><span class="sr-only">60% Complete</span></div>
-          </div>
-          <div class="progress">
-            <span>AAF &nbsp;&nbsp;&nbsp; 120 orders</span>
-            <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"><span class="sr-only">60% Complete</span></div>
-          </div>
-          <div class="progress">
-            <span>ZUK &nbsp;&nbsp;&nbsp; 100 orders</span>
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 30%;"><span class="sr-only">60% Complete</span></div>
+        <div class="col-md-4 col-sm-12">
+          <div class="panel panel-primary">
+            <div class="panel-heading">Merchant Balance</div>
+            <div class="panel-body">
+              <p>{{ loading }}</p>
+              <ul class="list-group">
+                <li class="list-group-item" v-for="merchantBalance in merchantBalanceList">
+                  <span class="badge" v-if="merchantBalance.balance > 0" v-bind:style="{ 'background-color': '#5cb85c' }">
+                  {{ merchantBalance.currency_id }} {{ merchantBalance.balance}}
+                  </span>
+                  <span class="badge" v-else="merchantBalance.balance < 0" v-bind:style="{ 'background-color': '#f0ad4e' }">
+                  {{ merchantBalance.currency_id }} {{ merchantBalance.balance}}
+                  </span>
+                  {{ merchantBalance.merchant_id }}
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <footer-bar></footer-bar>
   </div>
 </template>
 <script>
+  let API_URL = process.env.API_URL
   import NavBar from './common/NavBar.vue'
   import FooterBar from './common/FooterBar.vue'
   export default {
     components: {
       NavBar,
       FooterBar
+    },
+    data () {
+      return {
+        loading: 'Loading, Please wait a moment',
+        all_paid_orders_count: '',
+        pending_paid_orders_count: '',
+        allocated_orders_count: '',
+        merchant_pending_orders_count: [],
+        merchant_all_paid_orders_count: [],
+        merchant_allocated_orders_count: [],
+        merchantBalanceList: []
+      }
+    },
+    created () {
+      this.getMerchantBalanceList()
+      this.getDashBoardData()
+    },
+    methods: {
+      getMerchantBalanceList () {
+        this.$http.get(API_URL + 'merchant-balance')
+          .then(function (response) {
+            this.merchantBalanceList = response.data.data
+            this.loading = ''
+          })
+          .catch(function () {
+            this.loading = 'Load Error, You can refresh later'
+          })
+      },
+      getDashBoardData () {
+        this.$http.get(API_URL + 'fulfillment-order-dashboard')
+          .then(function (response) {
+            this.all_paid_orders_count = response.data.all_paid_orders_count
+            this.pending_paid_orders_count = response.data.pending_paid_orders_count
+            this.allocated_orders_count = response.data.allocated_orders_count
+            this.merchant_pending_orders_count = response.data.merchant_pending_orders_count
+            this.merchant_all_paid_orders_count = response.data.merchant_all_paid_orders_count
+            this.merchant_allocated_orders_count = response.data.merchant_allocated_orders_count
+            this.loading = ''
+          })
+          .catch(function () {
+            this.loading = 'Load Error, You can refresh later'
+          })
+      }
     }
   }
 </script>
